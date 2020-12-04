7ed1a3c70fe848a94fd94292c1d6b7b7c7b68816
Sam54123 <thesam54123@gmail.com>
2020-12-03 19:24:31 -0800

    added base motion recorder class

--- NeosMotionCapture/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NeosMotionCapture
-{
-    public class Class1
-    {
-    }
-}
---
+++ NeosMotionCapture/MotionRecorder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FrooxEngine;
+
+/// <summary>
+/// Component responsible for performing motion capture on a list of 
+/// </summary>
+namespace NeosMotionCapture
+{
+    [Category("transform")]
+    class MotionRecorder : Component
+    {
+        /// <summary>
+        /// All of the children of these slots will get recorded.
+        /// </summary>
+        public readonly SyncRefList<Slot> RecordedSlots;
+    }
+}
